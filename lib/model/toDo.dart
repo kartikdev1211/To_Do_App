333bf2af02648d3d823f9e9e6a5e693aa6b0e831
Kartik <https://github.com/kartikdev1211/Amazon_Clone.git>
2023-01-23 13:10:55 +0530

    Commited

---
+++ lib/model/toDo.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: file_names
+
+class ToDo {
+  String? id;
+  String? toDoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.toDoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', toDoText: 'Morning Excercise', isDone: true),
+      ToDo(id: '02', toDoText: 'Buy Groceries', isDone: true),
+      ToDo(
+        id: '03',
+        toDoText: 'Check Emails',
+      ),
+      ToDo(
+        id: '04',
+        toDoText: 'Team Meeting',
+      ),
+      ToDo(
+        id: '05',
+        toDoText: 'Work on mobile apps for 2 hour',
+      ),
+      ToDo(
+        id: '06',
+        toDoText: 'Dinner with family',
+      ),
+    ];
+  }
+}
